92587bcf380a6789e2cfd84bd0d30632765ada53
Marcin Ulman <marcin@ulman@dev>
2019-09-28 09:41:51 +0200

    init discover server

---
+++ src/main/java/dev/ulman/dmsdiscoverserver/DmsDiscoverServerApplication.java
@@ -0,0 +1,13 @@
+package dev.ulman.dmsdiscoverserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmsDiscoverServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmsDiscoverServerApplication.class, args);
+	}
+
+}
